58a4d5c9da2ef0d565139eff11c7487e67d2a61d
macair <121397835+blueminktech@users.noreply.github.com>
2023-07-29 00:45:31 +0700

    [FIX] update pnd3 and 1

---
+++ base_module/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Module',
+    'category': 'Base',
+    'license': 'OPL-1',
+    "depends": [
+        "base",
+    ],
+    "data": [
+        'views/module_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
